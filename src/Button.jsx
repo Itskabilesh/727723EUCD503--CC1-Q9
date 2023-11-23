1ebfff3a7aded8e4c91ad4af4da83c9610b764c0
Itskabilesh <151618357+Itskabilesh@users.noreply.github.com>
2023-11-23 09:53:07 +0530

    Add files via upload

---
+++ src/Button.jsx
@@ -0,0 +1,21 @@
+const FontSizeAdjuster = ({ defaultSize }) => {
+    const [fontSize, setFontSize] = useState(defaultSize);
+  
+    const increaseFontSize = () => {
+      setFontSize(fontSize + 2);
+    };
+  
+    const decreaseFontSize = () => {
+      setFontSize(fontSize - 2);
+    };
+  
+    return (
+      <div>
+        <p style={{ fontSize: ${fontSize}px, margin: '10px 0'}}>
+          this is a text sample
+        </p>
+        <button onClick={increaseFontSize}>Increase</button>
+        <button onClick={decreaseFontSize}>Decrease</button>
+      </div>
+    );
+  };
